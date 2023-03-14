249dd83f82bc0cff900f4c0c2980441a77c1ff07
icedit <radityaakmal25@student.uns.ac.id>
2023-03-14 09:52:43 +0700

    menambahkan exercise_01.py

---
+++ Exercise_01.py
@@ -0,0 +1,8 @@
+#Tugas Prokom Raditya Akmal 
+
+
+print("Raditya Akmal Putra Isnanto")
+print("I0322100")
+
+print(' Saya berasal dari kota Sidoarjo Jawa Timur saya berkuliah di UNS, Sidoarjo terkenal dengan makanan petis dengan bandeng dan udang yang menjadi lambang nya, Sidoarjo juga terkenal dengan bencana alam yaitu lumpur lapindo. ')
+print(' Harapan saya sebagai lulusan teknik industri adalah saya dapat mengimplementasikan ilmu saya terhadap kemajuan teknik industri di Indonesia supaya Industri di Indonesia lebih maju dan berkembang.')
